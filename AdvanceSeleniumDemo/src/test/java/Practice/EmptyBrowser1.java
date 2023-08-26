797b03fd4ed7895b26a70360d45c05d0f5e3ef58
Dell <Dell@AdarshMP>
2023-08-26 13:05:02 +0530

    push1

---
+++ AdvanceSeleniumDemo/src/test/java/Practice/EmptyBrowser1.java
@@ -0,0 +1,31 @@
+package Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EmptyBrowser1 {
+
+	public static void main(String[] args) throws Throwable {
+	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://localhost:8888/");
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+		Thread.sleep(3000);
+		
+		WebElement element = driver.findElement(By.xpath("//img[@src='themes/softed/images/user.PNG']"));
+		Actions act = new Actions(driver);
+		act.moveToElement(element).perform();
+	
+		driver.findElement(By.xpath("//a[text()='Sign Out']")).click();
+		//updated successfully
+	}
+
+}
